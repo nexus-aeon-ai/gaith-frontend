196ad16a2194975179980205f930dc517b99fff2
Afaq Cortech <afaq.ahmed@cortechsols.com>
2025-09-26 16:54:42 +0500

    feat: add form for create new lead

--- src/app/[locale]/(dashboard)/leads/page.tsx
+++ src/app/[locale]/(dashboard)/leads/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { ChevronLeft, ChevronRight, CirclePlus, EllipsisVertical, Search } from "lucide-react";
+import Link from "next/link";
 import { useTheme } from "next-themes";
 import { useState } from "react";
 
@@ -22,6 +23,7 @@ import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 
 import FilterSheet from "../../../../components/sheet/Filter";
+
 
 interface Client {
   id: string;
@@ -341,19 +343,21 @@ const LeadsPage = () => {
             Track and manage sales prospects through the conversion pipeline.
           </p>
         </div>
-        <Button
-          className={cn(
-            "flex items-center gap-1 sm:gap-2",
-            "bg-[#3072C0] rounded-[16px] w-full sm:w-auto",
-            "px-3 sm:px-4 lg:px-6 h-9 sm:h-10 lg:h-12",
-            "hover:bg-blue-700 text-white",
-            "text-xs sm:text-sm lg:text-base",
-          )}
-        >
-          <CirclePlus className="w-3 h-3 sm:w-4 sm:h-4" />
-          <span className="hidden sm:inline">Add New Lead</span>
-          <span className="sm:hidden">Add Lead</span>
-        </Button>
+        <Link href={"leads/new"}>
+          <Button
+            className={cn(
+              "flex items-center gap-1 sm:gap-2",
+              "bg-[#3072C0] rounded-[16px] w-full sm:w-auto",
+              "px-3 sm:px-4 lg:px-6 h-9 sm:h-10 lg:h-12",
+              "hover:bg-blue-700 text-white",
+              "text-xs sm:text-sm lg:text-base",
+            )}
+          >
+            <CirclePlus className="w-3 h-3 sm:w-4 sm:h-4" />
+            <span className="hidden sm:inline">Add New Lead</span>
+            <span className="sm:hidden">Add Lead</span>
+          </Button>
+        </Link>
       </div>
 
       {/* Search and Actions Section */}
