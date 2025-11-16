803a1e12c2b413cde46ee0480b62369a762efed9
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2025-11-16 23:47:22 +0200

    Integrate Prettier with ESLint and update config
    Added Prettier and eslint-config-prettier to dev dependencies and updated ESLint configuration to delegate formatting rules to Prettier. Adjusted VSCode settings to remove organizeImports on save. Minor import reordering in FilterSheet.tsx for consistency.

--- src/components/support/FilterSheet.tsx
+++ src/components/support/FilterSheet.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useQuery } from "@tanstack/react-query";
-import { useTheme } from "next-themes";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useTheme } from "next-themes";
 import { useRef } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
